7486383a20db6bd0f0217a9eb5f7fd7251ce01ad
Alex Candelario <candtech@pop-os.localdomain>
2021-05-20 20:59:58 -0400

    clean up

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.login(process.env.TOKEN)
+require('dotenv').config()
 
+client.login(process.env.TOKEN)
+console.log("tokn::",process.env.TOKEN)
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
-    const channel = client.channels.cache.get('');
+    const channel = client.channels.cache.get(process.env.CHANNEL_ID);
 
     if (!channel) return console.error('channel does not exist');
     channel.join().then(connection => {
-        connection.on('speaking', (user) => {
-            console.log(user);
+        connection.on('speaking', (_) => {
             const dispatch = connection.play(require('path').join(__dirname, process.env.AUDIO_FILE_PATH))
             dispatch.on('start', () => {
                 dispatch.setVolume(0.7);
@@ -24,15 +25,3 @@ client.once('ready', () => {
     }).catch(e => console.error(e))
 });
 
-
-
-
-client.on("voiceStateUpdate", (oldState, newState) => {
-    console.log('er')
-    // Do anything here with the updated state.
-    if(oldState.speaking !== newState.speaking) {
-ear
-        console.log('someone')
-       // User started speaking
-    }
-});
