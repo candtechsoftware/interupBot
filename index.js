86a21fc8dc1a4039cef5723a6dce6bc347411e54
Alex Candelario <candtech@pop-os.localdomain>
2021-05-20 21:01:30 -0400

    clean up

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const client = new Discord.Client();
 require('dotenv').config()
 
 client.login(process.env.TOKEN)
-console.log("tokn::",process.env.TOKEN)
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
     const channel = client.channels.cache.get(process.env.CHANNEL_ID);
